1e288e3c3f53482c677be1f20796d81d21f7cc4f
Kamol <tskamol@yandex.ru>
2020-08-19 12:40:14 +0300

    Домашка 4

--- lesson_for.php
+++ lesson_for.php
@@ -1,37 +1,31 @@
 <?php
 $photo = scandir('bmw/');
-/*var_dump($photo)*/;
-//
-/*if(isset($_FILES['image'])){
-    $errors= array();
-    $file_name = $_FILES['image']['name'];
-    $file_size = $_FILES['image']['size'];
-    $file_tmp = $_FILES['image']['tmp_name'];
-    $file_type = $_FILES['image']['type'];
-    $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+// Логирование в файл
 
-    $expensions= array("jpeg","jpg","png");
+define(DIR_LOGS, 'logs');
 
-    if(in_array($file_ext,$expensions)=== false){
-        $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-    }
+function logs () {
+//$file = fopen('test.log', 'a');
+$date = date('Y-m-d H:i:s');
+//fwrite($file,"$date\n");
+    file_put_contents(DIR_LOGS . '/log.txt', $date . "\r\n", FILE_APPEND);
 
-    if($file_size > 2097152) {
-        $errors[]='File size must be excately 2 MB';
-    }
 
-    if(empty($errors)==true) {
-        move_uploaded_file($file_tmp,"bmw/".$file_name);
-        echo "Success";
-    }else{
-        print_r($errors);
-    }
-}*/
-$uploads_dir = 'bmw/';
-$uploadfile = $uploads_dir . basename($_FILES['userfile']['name']);
+$file_arr = file(DIR_LOGS . "/log.txt");
+$lines = count($file_arr);
 
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Файл загружен";}
+if($lines>=10){
+   $dir = __DIR__ . '/' . DIR_LOGS . '/';
+   $count = count(scandir($dir)) - 2;
+   rename($dir . 'log.txt', $dir . 'log' . $count . '.txt');
+}
+
+}
+logs();
+
+// Написать функцию получения всех папок и файлов директории.
+$dire = scandir('.');
+var_dump($dire);
 
 ?>
 <!doctype html>
@@ -41,28 +35,23 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="javascript.js"></script>
-    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-<button id="btn_modal_window">Open Modal</button>
-<div id="my_modal" class="modal">
-    <div class="modal_content">
-        <span class="close_modal_window">×</span>
-         <p>Модальное окно!</p>
 
-    </div>
-</div>
 <?php
 foreach ($photo as $value) {
 
     echo "<a href='bmw/$value'> <img src='bmw/$value' style='width: 400px'></a>" ;
 }
 
+$uploads_dir = 'bmw/';
+$uploadfile = $uploads_dir . basename($_FILES['userfile']['name']);
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "Файл загружен";}
 ?>
 <form enctype="multipart/form-data"  method="POST">
-    <!--<input type = "file" name = "image" />-->
     <input name="userfile" type="file" />
     <input type="submit" value="Отправить файл" />
 </form>
